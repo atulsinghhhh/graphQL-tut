9c1bdc0079315feeab8a6700f4eab83be7c98c84
atulsinghhhh <atulsinghhh25@gmail.com>
2025-09-15 16:14:23 +0530

    added the getUserByid details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,21 +12,49 @@ const GET_USERS = gql`
   }
 `;
 
-function App() {
-  const { data, error, loading } = useQuery(GET_USERS);
+const GET_USER_BY_ID = gql`
+  query GETUserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      name
+      age
+      isMarried
+    }
+  }
+`;
 
-  if (loading) return <p>Data loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+function App() {
+  const { data: getUserData, error: getUserError, loading: getUserLoading } = useQuery(GET_USERS);
+  const { data: getUserByIdData, error: getUserByError, loading: getUserByLoading } = useQuery(GET_USER_BY_ID, {
+    variables: { id: "4" },
+  });
+
+  if (getUserLoading) return <p>Data loading...</p>;
+  if (getUserError) return <p>Error: {getUserError.message}</p>;
 
   return (
     <>
       <h1>Users</h1>
       <div>
-        {data.getUsers.map((user) => (
+        <h2>Chosen User:</h2>
+        {getUserByLoading && <p>Loading user...</p>}
+        {getUserByError && <p>Error: {getUserByError.message}</p>}
+        {getUserByIdData?.getUserById && (
+          <div>
+            <p>Name: {getUserByIdData.getUserById.name}</p>
+            <p>Age: {getUserByIdData.getUserById.age}</p>
+            <p>Married: {getUserByIdData.getUserById.isMarried ? "Yes" : "No"}</p>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2>All Users:</h2>
+        {getUserData?.getUsers.map((user) => (
           <div key={user.id}>
             <p>Name: {user.name}</p>
             <p>Age: {user.age}</p>
-            <p>Is this user Married: {user.isMarried ? "yes" : "no"}</p>
+            <p>Is this user Married: {user.isMarried ? "Yes" : "No"}</p>
           </div>
         ))}
       </div>
